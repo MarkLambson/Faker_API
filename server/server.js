b4053b4aee48e30756e127c9b3cdbd16dc754c36
MarkLambson <mj.lambson@yahoo.com>
2023-05-21 10:34:23 -0700

    .env

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 8000;
+const port = process.env.ATLAS_PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
